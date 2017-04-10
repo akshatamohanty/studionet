bb31cd35db72bdc11d572e3a9b5dd41711bbc717
akshatamohanty <akshatamohanty@gmail.com>
2017-04-10 18:47:43 +0800

    forum stagger working

--- public/user/js/ForumController.js
+++ public/user/js/ForumController.js
@@ -12,13 +12,66 @@ angular.module('studionet')
 											return;
 
 
+										/*$scope.list = [
+										  {
+										    "id": 1,
+										    "title": "node1",
+										    "collapsed": true,
+										    "nodes": [
+										      {
+										        "id": 11,
+										        "title": "node1.1",
+										        "nodes": [
+										          {
+										            "id": 111,
+										            "title": "node1.1.1",
+										            "nodes": []
+										          }
+										        ]
+										      },
+										      {
+										        "id": 12,
+										        "title": "node1.2",
+										        "nodes": []
+										      }
+										    ]
+										  },
+										  {
+										    "id": 2,
+										    "title": "node2",
+										    "nodrop": true,
+										    "nodes": [
+										      {
+										        "id": 21,
+										        "title": "node2.1",
+										        "nodes": []
+										      },
+										      {
+										        "id": 22,
+										        "title": "node2.2",
+										        "nodes": []
+										      }
+										    ]
+										  },
+										  {
+										    "id": 3,
+										    "title": "node3",
+										    "nodes": [
+										      {
+										        "id": 31,
+										        "title": "node3.1",
+										        "nodes": []
+										      }
+										    ]
+										  }
+										]*/
 										var nodes_list = GraphService.graph.nodes();
 										for(var i=0; i < nodes_list.length; i++){
 
 											var node = nodes_list[i];
 											$scope.nodeHash[ node.id ] = node;
 
-											node.data('nodes', node.incomers().map(function(c){ return c.data(); }));
+											node.data('nodes', node.incomers().nodes().map(function(c){ return c.data(); }));
 
 											if(node.outgoers().length == 0)
 												$scope.list.push( node.data() );
@@ -29,59 +82,6 @@ angular.module('studionet')
 								    });
 
 
-									/*$scope.list = [
-									  {
-									    "id": 1,
-									    "title": "node1",
-									    "collapsed": false,
-									    "nodes": [
-									      {
-									        "id": 11,
-									        "title": "node1.1",
-									        "nodes": [
-									          {
-									            "id": 111,
-									            "title": "node1.1.1",
-									            "nodes": []
-									          }
-									        ]
-									      },
-									      {
-									        "id": 12,
-									        "title": "node1.2",
-									        "nodes": []
-									      }
-									    ]
-									  },
-									  {
-									    "id": 2,
-									    "title": "node2",
-									    "nodrop": true,
-									    "nodes": [
-									      {
-									        "id": 21,
-									        "title": "node2.1",
-									        "nodes": []
-									      },
-									      {
-									        "id": 22,
-									        "title": "node2.2",
-									        "nodes": []
-									      }
-									    ]
-									  },
-									  {
-									    "id": 3,
-									    "title": "node3",
-									    "nodes": [
-									      {
-									        "id": 31,
-									        "title": "node3.1",
-									        "nodes": []
-									      }
-									    ]
-									  }
-									]*/
 
 
 
