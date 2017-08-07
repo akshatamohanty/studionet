2c0f35a759db0490c6ee36796a36bdaee6a0cfed
maurzcitrus <maurz.citrus@gmail.com>
2017-08-08 02:39:03 +0800

    Hashmap for tagCloud and ListView for forked

--- public/user/components/profile/profileViewController.js
+++ public/user/components/profile/profileViewController.js
@@ -15,13 +15,13 @@ angular.module('studionet')
           ];
 
       // todo: compute most popular contribution of the user
-      $scope.topcontribution = $scope.user.contributions[0];
+     /* $scope.topcontribution = $scope.user.contributions[0];*/
 
-        var computeTopContribution = function(){
+        $scope.topcontribution = function(){
 
             for(var i=0; i < $scope.user.contributions.length; i++){
               var topcontribution = $scope.user.contributions[i];
-              if ($scope.topcontributiontemp.likes >=5 && $scope.topcontributiontemp.bookmarks >=3) {
+              if ($scope.topcontribution[i].likes >=4 && $scope.topcontribution[i].bookmarks >=3) {
                 $scope.topcontribution = $scope.user.contributions[i];
               }
               else{
@@ -38,14 +38,53 @@ angular.module('studionet')
         if(numberofcomments.type == "comments"){
             countofcomments++;
         }
-
       }
 
 
       // todo: compute the most popular tags used by the user using the user.contributions
       // count the number of times the user has used a particular tag and reassign $scope.data
       var all_posts = $scope.user.contributions;
-      $scope.data = [
+
+      /*generate a hashmap from the array*/
+      var hashmap = {};
+      all_posts.forEach(function(element) {
+
+          if(hashmap[element.id]!==null && hashmap[element.id]!=undefined){
+             if(!Array.isArray(hashmap[element.id])){
+                 var tempObj = hashmap[element.tags];
+
+                 // json.stringify your object if you
+                 // want to  serialise your hashmap for external use
+
+                 hashmap[element.id] = [tempObj];
+             }
+
+             hashmap[element.id].push(element);
+
+          }
+          else{
+              // if you want to serialise your hashmap for external use
+              hashmap[element.id] = JSON.stringify(element);
+              // if not, you could just do without JSON.stringify
+              hashmap[element.id] = element;
+          }
+
+      });
+      console.log(hashmap);
+
+      /*     var tagCountArray = [];
+      for(var i=0; i< $scope.user.contributions.length; i++){
+          tagCountArray = $scope.user.contributions[i];
+        for(var j=0; j< tagCountArray.tags.length; j++){
+            var map = tagCountArray.tags.reduce(function(prev, cur) {
+            prev[cur] = (prev[cur] || 0) + 1;
+            return prev;
+            }, {});
+        }
+      }
+      console.log(map);*/
+
+          $scope.data = [
             {text: "Ipsum", weight: 9},  // three properties - id (tag id), text (tag name), weight (count of tag in the users' posts)
             {text: "Dolor", weight: 6},
             {text: "Sit", weight: 7},
